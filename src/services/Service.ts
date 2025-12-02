60473a32ce8ec01d1dbcfd74609d474c35d25543
sofia-araujo <sofiamarcelino2804@gmail.com>
2025-12-02 17:14:55 -0300

    :rocket: Deploy do projeto"

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://farmacia-ug0p.onrender.com/"
+    baseURL: import.meta.env.VITE_API_URL,
   })
   
   export const listar = async(url: string, setDados: Function) => {
